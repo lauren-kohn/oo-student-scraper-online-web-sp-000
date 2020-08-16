79bf65476339ef26c6ff4e8772bc0b2f50949b26
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-16 17:28:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     student_array = []
     doc = Nokogiri::HTML(open("#{index_url}"))
-    students = doc.css("div.student-card")
+    students = doc.css(".student-card")
     students.each do |student|
       hash = { name: student.css("student-name").text,
                location: student.css("student-location").text,
