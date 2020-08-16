9fc047bb908ee7cfa9d6d7f9c7bfd1d3cb4404a0
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-16 21:07:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      student.each do |name, location|
+      student.each do |key, value|
         
         self.send(("#{key}="), value)
       end
