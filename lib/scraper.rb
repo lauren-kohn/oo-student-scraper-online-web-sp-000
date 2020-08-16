938087b548f20c4194436147d8ec333f187e594a
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-16 20:12:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -37,7 +37,7 @@ class Scraper
     end
     
     student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote").text
-    student[:bio] = profile_page.css(".div.bio-content.content-holder div.description-holder p").text if profile_page.css(".div.bio-content.bio-holder div.description-holder p").text
+    student[:bio] = profile_page.css(".div.bio-content.content-holder div.description-holder p").text if profile_page.css(".div.bio-content.content-holder div.description-holder p").text
     
     student
   end
