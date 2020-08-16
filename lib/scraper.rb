706dd52429ba401d4e63ad67168aade0675af0ba
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-16 15:26:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,10 @@ require 'pry'
 class Scraper
   
   def self.scrape_index_page(index_url)
+    student_hash = {}
     students = Nokogiri::HTML(open('https://learn-co-curriculum.github.io/student-scraper-test-page/index.html'))
     #binding.pry
-    student_hash = students.css("div.student-card").each do |student|
+    students.css("div.student-card").each do |student|
       #student = Student.new 
       student_hash[name] = student.css("student-name").text
       student_hash[location] = student.css("student-location").text
